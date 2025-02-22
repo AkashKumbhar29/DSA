37b4fa41aeca3887ac8e1ef9dc30de0a37b5e475
AkashKumbhar29 <akashkumbhar32197@gmail.com>
2025-02-22 11:13:08 +0530

    Primitive file

---
+++ primitives.java
@@ -0,0 +1,20 @@
+public class Primitives {
+    public static void main(String[] args){
+
+            int numbers = 2; //Storage capacity is 4 byte
+            char word = 'A';
+            long largeint = 245632667746L; //8 bytes
+            float marks = 99.69f;  //4 bytes
+            double large_float_no = 22.23456754; //8 bytes
+            boolean check = true ;
+
+            System.out.println(numbers);
+            System.out.println(word);
+            System.out.println(largeint);
+            System.out.println(marks);
+            System.out.println(large_float_no);
+            System.out.println(check);
+
+    }
+    
+}
