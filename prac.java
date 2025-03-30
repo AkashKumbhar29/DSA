5612feacb304c1f8dcfc4ec51c9760bd380eac1c
AkashKumbhar29 <akashkumbhar32197@gmail.com>
2025-03-30 12:57:39 +0530

    again ready to fail

---
+++ prac.java
@@ -0,0 +1,14 @@
+class prac
+{
+    public static void main(String a[])
+    {
+        //Print any statement
+        System.out.println("Learning java");
+    //store value in variable
+  //  {
+        int n = 1;
+        int b = 2;
+        int result = n+b;
+        System.out.println("The result is :"+result);
+    }
+}
