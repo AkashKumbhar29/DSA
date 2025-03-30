43296b7c29e5718e449ff184d0652833f435fbdf
AkashKumbhar29 <akashkumbhar32197@gmail.com>
2025-03-30 15:42:05 +0530

    again ready to fail_1

--- prac.java
+++ prac.java
@@ -5,10 +5,21 @@ class prac
         //Print any statement
         System.out.println("Learning java");
     //store value in variable
-  //  {
+  /* 
         int n = 1;
         int b = 2;
         int result = n+b;
         System.out.println("The result is :"+result);
-    }
+   */
+        //casting is explicit conversion of the data type
+         float m =5.6f;
+         int g = (int)m;
+         System.out.println(m);
+         System.out.println("Converted to int "+g);
+     
+         //The logic used here is modulus as the limit of byte is 256 and as we wanted to convert a 258bits into byte so it converts like 258%256=2
+         int l = 258;
+         byte k = (byte)l;
+         System.out.println("onverted int to byte :" + k);
+        }
 }
